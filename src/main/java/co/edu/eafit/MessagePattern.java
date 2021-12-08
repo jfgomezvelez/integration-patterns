384d6205d1c88d67ff8abfa332be0a248c38aefa
Juan Felipe Gomez V <juan.gomez@sofka.com.co>
2021-12-08 15:13:31 -0500

    se implementa message pattern

---
+++ src/main/java/co/edu/eafit/MessagePattern.java
@@ -0,0 +1,59 @@
+package co.edu.eafit;
+
+import lombok.extern.java.Log;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+
+@Log
+@Component
+public class MessagePattern {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @RabbitListener(bindings = @QueueBinding(value = @Queue(
+            value = "weather.request.queue"
+    ),
+            exchange = @Exchange(value = "weather.request.exchange", type = ExchangeTypes.TOPIC),
+            key = "weather.request")
+    )
+    public void receive(Message message) {
+        log.info("Recibiendo mensaje ".concat(new String(message.getBody())).concat(" messageId ").concat(message.getMessageProperties().getMessageId()));
+        send("{\n" +
+                "    \"queryCost\": 1,\n" +
+                "    \"latitude\": 6.24589,\n" +
+                "    \"longitude\": -75.5746,\n" +
+                "    \"resolvedAddress\": \"Medellín, Colombia\",\n" +
+                "    \"address\": \"Medellin,ANT,CO\",\n" +
+                "    \"timezone\": \"America/Bogota\",\n" +
+                "    \"tzoffset\": -5.0,\n" +
+                "    \"description\": \"Similar temperatures continuing with a chance of rain multiple days.\"\n" +
+                "}", message.getMessageProperties().getMessageId());
+    }
+
+    public void send(String message, String messageId) {
+        log.info("Enviando evento "
+                .concat(message)
+                .concat(" a ").concat("weather.response.exchange")
+                .concat(":")
+                .concat("weather.response"));
+
+        byte[] data = message.getBytes(StandardCharsets.UTF_8);
+
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.setMessageId(messageId);
+
+        Message messageToSend = new Message(data, messageProperties);
+        rabbitTemplate.convertAndSend("weather.response.exchange", "weather.response", messageToSend);
+    }
+}
